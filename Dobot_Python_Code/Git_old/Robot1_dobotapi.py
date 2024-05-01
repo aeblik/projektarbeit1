f0a63345c100314c3bad93bc9bea1d157448057e
David Senn <dave-senn@hotmail.com>
2024-05-01 21:22:22 +0200

    Robot1 updated

--- Dobot_Python_Code/Git_old/Robot1_dobotapi.py
+++ Dobot_Python_Code/Git_old/Robot1_dobotapi.py
@@ -13,7 +13,7 @@ bot.connect()
 bot.clear_alarms()
 
 # Define the x, y, z, r coordinates for various positions
-home_position = (225.6311, -191.7791, -6.6714, 55.6912)
+home_position = (200.0, 0.0, 80.0, 25.0)
 
 gripper_positions = {
     'above': (116.3743, -194.0146, 7.5, 57.408),
@@ -22,62 +22,64 @@ gripper_positions = {
 }
 
 pallet_positions = {
-    '1': {'front': (158.6421, -183.6389, -6.6714, 55.6912),
-          'loaded': (225.6311, -183.6389, -6.6714, 55.6912),
-          'lift': (225.6311, -183.6389, 60.0, 55.6912)},
+    '1': {'front': (152.8339, -183.6392, -6.6714, 57.0592),
+          'loaded': (225.6311, -183.6389, -6.6714, 57.0592),
+          'lift': (225.6311, -183.6389, 60.0, 57.0592)},
     '2': {'front': (147.123, -44.3675, -5.9256, 59.5793),
-          'loaded': (227.8143, -44.3675, -5.9256, 59.5793),
-          'lift': (227.8143, -44.3675, 60.0, 59.5793)},
+          'loaded': (228.5, -44.3675, -5.9256, 59.5793),
+          'lift': (228.5, -44.3675, 60.0, 59.5793)},
     '3': {'front': (147.1234, 104.6730, -5.9256, 59.5793),
           'loaded': (226.6, 104.6730, -5.9256, 59.5793),
           'lift': (226.6, 104.6730, 60.0, 59.5793)},
 }
 
 loading_area_positions = {
-    'above': (136.901, -244.2901, 60.0, -31.4771),
-    'on': (136.901, -244.2901, 33.8178, -31.4771),
-    'empty_fork': (136.901, -144.3046, 33.8178, -31.4771),
-    'in_front': (136.901, -126.1611, 60.0, -31.4771),
+    'above': (136.901, -244.0, 60.0, -29.7491),
+    'on': (136.901, -244.0, 33.8178, -29.7491),
+    'empty_fork': (136.901, -144.3046, 33.8178, -29.7491),
+    'in_front': (136.901, -126.1611, 60.0, -29.7491),
 }
-
-# Function to move to a specified location
-def move_to(bot, position):
-    bot.move_to_Joint(*position)
 
 # Function to pick up and move a pallet to the loading area
 def move_pallet_to_loading_area(bot, pallet_num):
-    move_to(bot, pallet_positions[pallet_num]['front'])
-    move_to(bot, pallet_positions[pallet_num]['loaded'])
-    move_to(bot, pallet_positions[pallet_num]['lift'])
-    move_to(bot, loading_area_positions['above'])
-    move_to(bot, loading_area_positions['on'])
-    move_to(bot, loading_area_positions['empty_fork'])
-    time.sleep(5)  # Wait for unloading, replace with message to Bot 2
+    bot.move_to_Linear(*pallet_positions[pallet_num]['front'])
+    bot.move_to_Linear(*pallet_positions[pallet_num]['loaded'])
+    bot.move_to_Linear(*pallet_positions[pallet_num]['lift'])
+    bot.move_to_Linear(*loading_area_positions['above'])
+    bot.move_to_Linear(*loading_area_positions['on'])
+    print(f"Pallet {pallet_num} has been placed on the loading area.")
+    input("Please confirm that Bot 2 has completed unloading (press Enter to continue)...")
 
 # Function to return an empty pallet to storage
 def return_empty_pallet_to_storage(bot, pallet_num):
-    move_to(bot, loading_area_positions['on'])
-    move_to(bot, loading_area_positions['above'])
-    move_to(bot, loading_area_positions['in_front'])
-    move_to(bot, pallet_positions[pallet_num]['lift'])
-    move_to(bot, pallet_positions[pallet_num]['loaded'])
-    move_to(bot, pallet_positions[pallet_num]['front'])
+    bot.move_to_Linear(*loading_area_positions['on'])
+    bot.move_to_Linear(*loading_area_positions['above'])
+    bot.move_to_Linear(*loading_area_positions['in_front'])
+    bot.move_to_Joint(*pallet_positions[pallet_num]['lift'])
+    bot.move_to_Linear(*pallet_positions[pallet_num]['loaded'])
+    bot.move_to_Linear(*pallet_positions[pallet_num]['front'])
 
 # Function to attach the fork to gripper
 def attach_fork_to_gripper(bot):
-    move_to(bot, gripper_positions['above'])
-    move_to(bot, gripper_positions['pick'])
+    bot.move_to_Linear(*gripper_positions['above'])
+    bot.gripper.open()
+    bot.gripper.idle()
+    bot.move_to_Linear(*gripper_positions['pick'])
     bot.gripper.close()
-    move_to(bot, gripper_positions['lift'])
+    bot.gripper.idle()
+    bot.move_to_Linear(*gripper_positions['lift'])
 
 # Function to detach the fork from gripper
 def detach_fork_from_gripper(bot):
-    move_to(bot, gripper_positions['lift'])
-    move_to(bot, gripper_positions['pick'])
+    bot.move_to_Linear(*gripper_positions['lift'])
+    bot.move_to_Linear(*gripper_positions['pick'])
     bot.gripper.open()
-    move_to(bot, gripper_positions['above'])
+    bot.gripper.idle()
+    bot.move_to_Linear(*gripper_positions['above'])
 
 # Attach the fork to the gripper
+bot.gripper.open()
+bot.gripper.idle()
 attach_fork_to_gripper(bot)
 
 ###### Palette 1 ######
@@ -99,7 +101,7 @@ return_empty_pallet_to_storage(bot, '3')
 detach_fork_from_gripper(bot)
 
 # Return to the home position
-move_to(bot, home_position)
+bot.move_to_Linear(*home_position)
 
 # Properly close the connection and return to idle
 bot.close()  # Properly close the connection
